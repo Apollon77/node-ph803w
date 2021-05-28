a397a03649034d5d04b4c715314e7b7f26572ac0
Ingo Fischer <github@fischer-ka.de>
2021-05-28 08:26:39 +0200

    * make sure error event is subscribed at the beginning in example

--- example/data.js
+++ example/data.js
@@ -2,16 +2,17 @@ const { PH803WDevice } = require('../index');
 
 async function main() {
     const device = new PH803WDevice(process.argv[2]);
+    device.on('error', err => {
+        console.log('Error: ' + err);
+    });
+    device.on('data', data => {
+        console.log('Data: ' + JSON.stringify(data));
+    });
+
     await device.connect();
 
     await device.login();
 
-    device.on('data', data => {
-        console.log('Data: ' + JSON.stringify(data));
-    });
-    device.on('error', err => {
-        console.log('Error: ' + err);
-    });
     await device.retrieveData();
 }
 
