1d79c517b813accb84ca4a6350044e2a546415f3
Ingo Fischer <github@fischer-ka.de>
2021-06-08 09:23:24 +0200

    * update discovery and add tests

---
+++ test/lib/testUdpServer.js
@@ -0,0 +1,79 @@
+const dgram = require('dgram');
+const debug = require('debug')('ph803w:testUdpServer');
+
+const DEFAULT_UDP_PORT = 12414;
+
+const requestResponse = {
+    '0000000303000003': Buffer.from('00000003680000040016434671704a5453796d434539504c6c703144706268590006483fda87dc4700000020326433643935346439626237343162346131396261313135333130343933326200000000000000026170692e67697a776974732e636f6d3a383000342e302e3800', 'hex')
+};
+
+class TestServer {
+    constructor(options) {
+        this.options = options || {};
+
+        this.lastDataPackets = [];
+        this.lastPingTime = null;
+        this.server = null;
+    }
+
+    open() {
+        let promiseResponded = false;
+        return new Promise((resolve, reject) => {
+            this.server = dgram.createSocket({type: 'udp4', reuseAddr: true});
+
+            this.server.on('message', (message, remote) => {
+                this.handleUdpData(message, remote);
+            });
+
+            this.server.on('error', err => {
+                debug('Can not Listen for UDP packages: ' + err);
+                if (!promiseResponded) {
+                    reject(err);
+                    promiseResponded = true;
+                }
+                this.emit('error', err);
+            });
+
+            this.server.bind(DEFAULT_UDP_PORT, undefined, () => {
+                if (!this.server) {
+                    return;
+                }
+                debug(`Listen for udp packages on port ${DEFAULT_UDP_PORT}`);
+
+                if (!promiseResponded) {
+                    resolve(true);
+                    promiseResponded = true;
+                }
+            });
+        });
+    }
+
+    close() {
+        return new Promise(resolve => {
+            this.server.close(() => {
+                debug('closed');
+                resolve(true);
+            });
+        });
+    }
+
+    handleUdpData(data, remote) {
+        debug(`udp packet received: ${data.toString('hex')} from ${remote.address}:${remote.port}`);
+        const dataStr = data.toString('hex');
+        this.lastDataPackets.push(dataStr);
+        if (requestResponse[dataStr]) {
+            debug(`response data: ${requestResponse[dataStr].toString('hex')}`);
+            try {
+                this.server.send(requestResponse[dataStr], 0, requestResponse[dataStr].length, remote.port, remote.address);
+            } catch (e) {
+                this.server.emit('error', e);
+            }
+        }
+    }
+
+    resetLastDataPackets() {
+        this.lastDataPackets = [];
+    }
+}
+
+module.exports = TestServer;
--- test/testDevice.js
+++ test/testDevice.js
@@ -3,54 +3,13 @@ const chai = require('chai');
 const expect = chai.expect;
 const TestServer = require('./lib/testServer');
 const PH803WDevice = require('../lib/device');
-const PH803WDiscovery = require('../lib/discovery');
 
-describe('PH803-W Test', function() {
+describe('PH803-W Device Test', function() {
     let testServer;
 
     before('init server', async () => {
         testServer = new TestServer();
         await testServer.open();
-    });
-
-    it('parse discovery message #1', done => {
-        const discovery = new PH803WDiscovery();
-
-        discovery.on('error', err => {
-            // todo
-        });
-
-        discovery.on('device', data => {
-            expect(data.id).to.equal('CFqpJTSymCE9PLlp1DpbhY');
-            expect(data.data3).to.equal('2d3d954d9bb741b4a19ba1153104932b');
-            expect(data.apiServer).to.equal('api.gizwits.com:80');
-            expect(data.version).to.equal('4.0.8');
-            expect(data.ip).to.equal('127.0.0.1');
-            done();
-        });
-
-        const data = Buffer.from('00000003680000040016434671704a5453796d434539504c6c703144706268590006483fda87dc4700000020326433643935346439626237343162346131396261313135333130343933326200000000000000026170692e67697a776974732e636f6d3a383000342e302e3800', 'hex');
-        discovery.parseResponse(data, {address: '127.0.0.1'});
-    });
-
-    it('parse discovery message #2', done => {
-        const discovery = new PH803WDiscovery();
-
-        discovery.on('error', err => {
-            // todo
-        });
-
-        discovery.on('device', data => {
-            expect(data.id).to.equal('2tqRa88FgaUS5AxgARVYug');
-            expect(data.data3).to.equal('2d3d954d9bb741b4a19ba1153104932b');
-            expect(data.apiServer).to.equal('api.gizwits.com:80');
-            expect(data.version).to.equal('4.1.2');
-            expect(data.ip).to.equal('127.0.0.2');
-            done();
-        });
-
-        const data = Buffer.from('00000003780000040016327471526138384667615553354178674152565975670006e8db848c4b17000830343032303033410020326433643935346439626237343162346131396261313135333130343933326200000000000000026170692e67697a776974732e636f6d3a383000342e312e32003033303330303030', 'hex');
-        discovery.parseResponse(data, {address: '127.0.0.2'});
     });
 
     it('connect and disconnect', async () => {
